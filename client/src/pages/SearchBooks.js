8e85c0a98ef442ddc4f16243db5e2485478eebee
Jacob Karla <jacob.l.karla@gmail.com>
2022-10-06 15:56:08 -0400

    refactored savedBooks to work with apollo, imported mutations

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -5,7 +5,7 @@ import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import {SAVE_BOOK} from '../utils/mutation';
-import {GET_ME} from '..utils/queries';
+
 
 const SearchBooks = () => {
   // create state for holding returned google api data
