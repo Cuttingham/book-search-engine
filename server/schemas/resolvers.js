3cc2597f869ee8c1642b0ae02437d067a2f276b6
Jacob Karla <jacob.l.karla@gmail.com>
2022-10-06 14:21:25 -0400

    fixed resolver name, added mutations and queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -42,7 +42,7 @@ const resolvers = {
             }
             throw new AuthenticationError("Unable to do that without being logged in!")
         },
-        deleteBook: async(parent,{bookId},context)=>{
+        removeBook: async(parent,{bookId},context)=>{
             if(context.user){
                 const user = await User.findOneAndUpdate(
                     {_id:context.user._id},
